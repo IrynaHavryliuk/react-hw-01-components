d06ef95618a9d5676721af74e3b9ef415b5da783
IrynaHavryliuk <99551348+IrynaHavryliuk@users.noreply.github.com>
2023-01-23 18:00:50 +0200

    add news

--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
-// import React from 'react';
-import ReactDOM from 'react-dom';
-import profiles from './user.json';
-import './profile.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from 'components/App';
 
-const data = profiles[0];
-
-const profile = (
-  <div class="profile">
-    <div class="description">
-      <img
-        src={data.avatar}
-        alt={data.tag}
-        class="avatar"
-      />
-      <p class="name">{data.username}</p>
-      <p class="tag">{data.tag}</p>
-      <p class="location">{data.location}</p>
-    </div>
-
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">{data.stats.followers}</span>
-      </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">{data.stats.views}</span>
-      </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">{data.stats.likes}</span>
-      </li>
-    </ul>
-  </div>);
-ReactDOM.render(profile, document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
