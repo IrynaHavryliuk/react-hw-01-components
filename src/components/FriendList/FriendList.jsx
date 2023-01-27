3b45f72eae854312f0578f4f7079a6f7c2f6ee47
IrynaHavryliuk <99551348+IrynaHavryliuk@users.noreply.github.com>
2023-01-27 16:37:54 +0200

    add

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,7 +1,7 @@
 import css from './FriendList.module.css';
 import PropTypes from 'prop-types';
 
-import { FriendListItem } from './FriendListItem/FriendListItem';
+import { FriendListItem } from './FriendListItem';
 
 export const FriendList = ({ friends }) => {
   return (
